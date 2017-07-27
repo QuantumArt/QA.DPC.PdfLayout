7741b7e8523eb851803c4d905bf0bf093911a713
EmelyanenkoM <EmelyanenkoM@quantumart.ru>
2017-07-27 23:34:07 +0300

    mapper in progress

--- controls/tariff_test/mapper.js
+++ controls/tariff_test/mapper.js
@@ -2,15 +2,38 @@ const _ = require('lodash');
 
 const mapData = (data) => {
   const tariff = data[0];
-  const { MarketingProduct, Description, ParamsDic } = tariff;
+  const {
+    MarketingProduct,
+    Description,
+    Parameters,
+    ParamsDic,
+  } = tariff;
+
+  const filteredParams = _.filter(Parameters, el => el.Group.Alias !== 'SystemGroup');
+  const baseParams = _
+    .chain(filteredParams)
+    .map(el => el.BaseParameter)
+    .compact()
+    .map(el => el.Title)
+    .uniq()
+    .value();
+  const mappedParams = _
+    .chain(filteredParams)
+    .filter(el => el.BaseParameter !== undefined)
+    .map((el, i) => {
+      console.log();
+    })
+    .value();
 
   const result = {
     head: {
-      title: MarketingProduct.Title,
-      description: Description,
+      tariffName: MarketingProduct.Title,
+      tariffDescription: Description,
     },
     body: {
-
+      params: {
+        baseParams,
+      },
     },
   };
 
@@ -23,10 +46,15 @@ module.exports = mapData;
   {
     header: {
       tariffName: string,
-
+      tariffDescription: string
     },
     body: {
-      
+      params: [
+        {
+          paramName: string,
+
+        }
+      ]
     }
   }
 */
