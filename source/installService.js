3f2290cf9d90cd9f85b69d834e91cf6e4209cfbb
Stepan Milyaev <thorgnir@gmail.com>
2017-09-04 16:16:46 +0300

    fix installservice script

--- source/installService.js
+++ source/installService.js
@@ -5,6 +5,10 @@ const svc = new Service({
   name: 'QA.DPC.Node.PdfGenerator',
   description: 'Generates html for DPC products',
   script: path.resolve('server.js'),
+  env: {
+    name: 'NODE_ENV',
+    value: 'production',
+  },
 });
 
 svc.on('install', () => {
