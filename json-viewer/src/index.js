e3bf0dd89c08694029c38c6daf4415df75f41117
Unknown <vozzic@gmail.com>
2017-10-22 06:36:01 +0300

    create-react-app removed

--- json-viewer/src/index.js
+++ json-viewer/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const render = (Component) => {
+  ReactDOM.render(<Component />, document.getElementById('root'));
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App);
+  });
+}
---
+++ json-viewer/webpack.config.dev.js
@@ -0,0 +1,76 @@
+/* eslint-disable import/no-extraneous-dependencies, global-require, no-undef, quote-props */
+const webpack = require('webpack');
+const path = require('path');
+const WebpackNotifierPlugin = require('webpack-notifier');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+console.log(process.env.NODE_ENV);
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch', // RHL patch
+    'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    './src/index.js', // Your appʼs entry point
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'json-viewer.bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [{
+      enforce: 'pre',
+      test: /\.(js|jsx)?$/,
+      exclude: /node_modules/,
+      loader: 'eslint-loader',
+      options: {
+        failOnError: true,
+      },
+    },
+    {
+      test: /\.(js|jsx)?$/,
+      use: ['react-hot-loader/webpack', 'babel-loader'],
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)?$/,
+      use: 'file-loader?name=[name].[ext]&outputPath=fonts/',
+    },
+    {
+      test: /\.(png|gif)?$/,
+      use: 'url-loader',
+    },
+    {
+      test: /\.svg$/,
+      use: 'url-loader',
+    },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new WebpackNotifierPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+    new CopyWebpackPlugin([{
+      from: './public',
+      to: './',
+    }]),
+  ],
+  devServer: {
+    port: 3000,
+    hot: true,
+    historyApiFallback: true,
+  },
+};
