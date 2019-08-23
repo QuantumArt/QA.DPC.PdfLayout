fe33816f0430c5912588a57fc1791bee02ca1908
Sergey Koryakin <KoryakinS@quantumart.ru>
2019-08-23 12:02:16 +0300

    #137591 pass customerCode param from request to pdfserver api

--- contentViewers/src/shared/api.js
+++ contentViewers/src/shared/api.js
@@ -8,23 +8,25 @@ export default function api() {
   const qParams = urlParams.split('&');
   const contentId = qParams.filter(el => el.includes('content_id='))[0].split('=')[1];
   const contentItemId = qParams.filter(el => el.includes('content_item_id='))[0].split('=')[1];
-  const baseUrl = window.apiUrl;
+  const customerCode = qParams.filter(el => el.includes('customerCode='))[0].split('=')[1];
   const category = 'test';
+  const commonParams = `category=${category}&customerCode=${customerCode}`;
+  const baseUrl = window.apiUrl;
 
   const isRoaming = contentId === contentTypes.roaming;
 
   if (isRoaming) {
     return {
-      json: `${baseUrl}/roamingJsonMapper/${contentItemId}?category=${category}`,
-      pdf: `${baseUrl}/roaming/?id=${contentItemId}&category=${category}&attachment=true`,
-      html: `${baseUrl}/roaming/?id=${contentItemId}&category=${category}&attachment=true&asHtml=true`,
+      json: `${baseUrl}/roamingJsonMapper/${contentItemId}?${commonParams}`,
+      pdf: `${baseUrl}/roaming/?id=${contentItemId}&${commonParams}&attachment=true`,
+      html: `${baseUrl}/roaming/?id=${contentItemId}&${commonParams}&attachment=true&asHtml=true`,
     };
   }
 
   return {
-    json: `${baseUrl}/productJsonMapper/${contentItemId}?category=${category}`,
-    pdf: `${baseUrl}/pdf/${contentItemId}?category=${category}&attachment=true`,
-    html: `${baseUrl}/pdf/${contentItemId}?category=${category}&attachment=true&asHtml=true`,
+    json: `${baseUrl}/productJsonMapper/${contentItemId}?${commonParams}`,
+    pdf: `${baseUrl}/pdf/${contentItemId}?${commonParams}&attachment=true`,
+    html: `${baseUrl}/pdf/${contentItemId}?${commonParams}&attachment=true&asHtml=true`,
   };
 }
 
