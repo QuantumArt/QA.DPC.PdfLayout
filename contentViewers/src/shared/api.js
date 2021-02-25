9f16e82411575ca158d0b7eba2ed21f0f8e48b03
Pavel Celut <CelutP@gmail.com>
2021-02-25 18:01:28 +0300

    Fix generation urls

--- contentViewers/src/shared/api.js
+++ contentViewers/src/shared/api.js
@@ -1,32 +1,21 @@
 export default function api() {
-  const contentTypes = {
-    products: '339',
-    roaming: '436',
-  };
 
   const urlParams = window.location.search.substring(1);
   const qParams = urlParams.split('&');
   const contentId = qParams.filter(el => el.includes('content_id='))[0].split('=')[1];
   const contentItemId = qParams.filter(el => el.includes('content_item_id='))[0].split('=')[1];
   const customerCode = qParams.filter(el => el.includes('customerCode='))[0].split('=')[1];
-  const category = 'test';
+  const categoryParam = qParams.filter(el => el.includes('category='));
+  const category = (categoryParam.length > 0) ? categoryParam[0].split('=')[1] : 'test';
+  const slugParam = qParams.filter(el => el.includes('slug='));
+  const slug = (slugParam.length > 0) ? slugParam[0].split('=')[1] : 'product';
   const commonParams = `category=${category}&customerCode=${customerCode}`;
   const baseUrl = window.apiUrl;
 
-  const isRoaming = contentId === contentTypes.roaming;
-
-  if (isRoaming) {
-    return {
-      json: `${baseUrl}/roamingJsonMapper/${contentItemId}?${commonParams}`,
-      pdf: `${baseUrl}/roaming/?id=${contentItemId}&${commonParams}&attachment=true`,
-      html: `${baseUrl}/roaming/?id=${contentItemId}&${commonParams}&attachment=true&asHtml=true`,
-    };
-  }
-
   return {
-    json: `${baseUrl}/productJsonMapper/${contentItemId}?${commonParams}`,
-    pdf: `${baseUrl}/pdf/${contentItemId}?${commonParams}&attachment=true`,
-    html: `${baseUrl}/pdf/${contentItemId}?${commonParams}&attachment=true&asHtml=true`,
+    json: `${baseUrl}/${slug}JsonMapper/${contentItemId}?${commonParams}`,
+    pdf: `${baseUrl}/${slug}/${contentItemId}?${commonParams}&attachment=true`,
+    html: `${baseUrl}/${slug}/${contentItemId}?${commonParams}&attachment=true&asHtml=true`,
   };
 }
 
